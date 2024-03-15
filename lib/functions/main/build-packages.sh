9ee3f8c915e5d50d498f4157cbd94c4d1f86e7e3
August <2819763+Dangku@users.noreply.github.com>
2024-03-15 12:14:32 +0800

    build armbian-config deb for minimal

--- lib/functions/main/build-packages.sh
+++ lib/functions/main/build-packages.sh
@@ -32,9 +32,9 @@ function determine_artifacts_to_build_for_image() {
 	fi
 
 	if [[ "${PACKAGE_LIST_RM}" != *armbian-config* ]]; then
-		if [[ $BUILD_MINIMAL != yes ]]; then
+		#if [[ $BUILD_MINIMAL != yes ]]; then
 			artifacts_to_build+=("armbian-config")
-		fi
+		#fi
 	fi
 
 	if [[ "${PACKAGE_LIST_RM}" != *armbian-zsh* ]]; then
